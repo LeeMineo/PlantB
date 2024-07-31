220d0bbfd1fb74ba10ef499334555d7210c74fd0
LeeMineo <lms4882@gmail.com>
2024-07-31 23:44:00 +0900

    확인

--- front/src/App.js
+++ front/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to My Simple React Page</h1>
         <p>This is a simple page deployed on Vercel.</p>
+        <p>어때 표시되지??.</p>
       </header>
     </div>
   );
