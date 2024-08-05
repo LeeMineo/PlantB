bfd44b8b5ffae475fddaf444f42b04376aefc8f4
LeeMineo <lms4882@gmail.com>
2024-08-05 12:54:08 +0900

    main 디자인 수정

--- front/src/sections/Main.js
+++ front/src/sections/Main.js
@@ -1,13 +1,17 @@
 // src/sections/Main.js
 import React from 'react';
 import '../css/Main.css';
+import Tagline from '../assets/svg/Tagline.svg';
 
 const Main = () => {
   return (
     <section id="main" className="main-section">
       <div className="overlay">
         <div className="content">
-          <h2 className="tagline"> --- PLAN B</h2>
+          <div className="function-title-main">
+              <img src={Tagline} alt="Tagline" className="tagline-main" />
+              <p className=".function-subtitle-main">PLAN B</p>
+          </div>
           <h1 className="title">Plant<span className="highlight">:B</span></h1>
           <p className="description">플랜트비와 함께 산길을 정복하며 식물도감을 채워보세요.</p>
           <div className="scroll-down">
