f2fd02e11988ebd74350b91a6a2b39e7d7df288c
LeeMineo <103713510+LeeMineo@users.noreply.github.com>
2024-08-12 14:54:57 +0900

    Delete appFront directory

--- appFront/index.web.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import appConfig from './app.json';
-
-AppRegistry.registerComponent(appConfig.name, () => App);
-AppRegistry.runApplication(appConfig.name, {
-  initialProps: {},
-  rootTag: document.getElementById('app-root'),
-});
