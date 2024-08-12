478e8ce82ea9235cbeb1d070eac8479c10d36797
LeeMineo <lms4882@gmail.com>
2024-08-12 15:27:59 +0900

    Create service-worker.js

---
+++ appfront/public/service-worker.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-restricted-globals */
+// public/service-worker.js
+
+self.addEventListener('install', event => {
+    console.log('Service Worker installing.');
+  });
+  
+  self.addEventListener('activate', event => {
+    console.log('Service Worker activating.');
+  });
+  
+  self.addEventListener('fetch', event => {
+    console.log('Fetching:', event.request.url);
+  });
+  
