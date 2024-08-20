7075d2af0eac35b6e1e17bfcd62007eaffaeefb5
LeeMineo <lms4882@gmail.com>
2024-08-20 23:34:30 +0900

    .

--- appfront/src/components/Camera/Analysis/AnalysisSummary.js
+++ appfront/src/components/Camera/Analysis/AnalysisSummary.js
@@ -1,33 +1,31 @@
 import React from 'react';
+import './AnalysisSummary.css';
 
 const AnalysisSummary = ({ plantData }) => {
     // plantData가 undefined거나 빈 배열이 아닌지 확인
     if (!plantData || plantData.length === 0) {
-        return <div>No data available for the given plant.</div>;
+        return <div id="analysissum-no-data">No data available for the given plant.</div>;
     }
 
     return (
-        <div className="analysis-summary">
-            <h2>Analysis Result</h2>
+        <div id="analysissum-summary">
+            <h2 id="analysissum-title">Analysis Result</h2>
             {plantData.map((result, index) => (
-                <div key={index} className="plant-item">
-                    <h3>
+                <div key={index} id="analysissum-plant-item" className="analysissum-plant-item">
+                    <h3 id="analysissum-species">
                         Species: {result.species?.commonNames?.join(', ') || result.species?.scientificNameWithoutAuthor}
                     </h3>
-                    <br></br>
-                    <p>Scientific Name: {result.species?.scientificName}</p>
-                    <br></br>
-                    <p>Family: {result.species?.family?.scientificName}</p>
-                    <br></br>
-                    <p>Score: {result.score}</p>
-                    <div>
-                        <h4>Images:</h4>
+                    <p id="analysissum-scientific-name">Scientific Name: {result.species?.scientificName}</p>
+                    <p id="analysissum-family">Family: {result.species?.family?.scientificName}</p>
+                    <p id="analysissum-score">Score: {result.score}</p>
+                    <div id="analysissum-images-container">
+                        <h4 id="analysissum-images-title">Images:</h4>
                         {result.images?.length > 0 ? (
                             result.images.map((image, idx) => (
-                                <img key={idx} src={image.url.m} alt={image.organ} />
+                                <img key={idx} id="analysissum-image" src={image.url.m} alt={image.organ} />
                             ))
                         ) : (
-                            <p>No images available</p>
+                            <p id="analysissum-no-images">No images available</p>
                         )}
                     </div>
                 </div>
